1961cb9c9f87f2b1ffde7124c1279c338f2f2b53
Eva Ratna Palupi <evaratnapalupi@outlook.com>
2020-07-08 00:07:12 +0700

    Menampilkan data produk

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function(req,res){
     response.ok("Aplikasi REST API ku berjalan yeee..",res)
 };
+
+//menampilkan semua data produk
+exports.tampilsemuaproduk = function (req, res) {
+    connection.query('SELECT * FROM tb_produk', function (error, rows, fileds) {
+        if (error) {
+            console.log(error);
+        } else {
+            response.ok(rows, res)
+        }
+    });
+};
