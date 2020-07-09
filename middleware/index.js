30cd957c899cf950f1a715c64e3a023d3b5b9453
Eva Ratna Palupi <evaratnapalupi@outlook.com>
2020-07-09 17:29:57 +0700

    Berhasil verifikasi Admin

--- middleware/index.js
+++ middleware/index.js
@@ -12,7 +12,6 @@ router.post('/api/v1/login', auth.login);
 //alamat yang perlu otorisasi
 router.get('/api/v1/rahasia', verifikasi(2), auth.halamanrahasia);
 
-
 router.post('/api/v1/tambah-produk', verifikasiAdmin(1), jsonku.tambahproduk);
 router.put('/api/v1/ubah-produk', verifikasiAdmin(1), jsonku.ubahproduk);
 router.delete('/api/v1/hapus-produk', verifikasiAdmin(1), jsonku.hapusproduk);
